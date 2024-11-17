f670804d18386fa7c780a0da05f54fdd0aba02e9
GodlikeGoose <aliislamdzafarov054@gmail.com>
2024-11-17 17:08:53 +0300

    модуль 2.3

--- homeworks/modul_2/modul_2_3.py
+++ homeworks/modul_2/modul_2_3.py
@@ -1,6 +1,8 @@
 i = 0
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 while i <len(my_list)-1 :
+    if my_list[i] == 0 :
+        continue
     if my_list[i] < 0 :
         break
     print(my_list[i])
