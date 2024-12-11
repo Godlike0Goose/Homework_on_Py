11d77144076768dba297d8555b586f8531f94ca8
GodlikeGoose <aliislamdzafarov054@gmail.com>
2024-12-11 18:01:34 +0300

    модуль и пакеты

---
+++ homeworks/m4/module_4_1.py
@@ -0,0 +1,17 @@
+from fake_math import divide as lol
+from true_math import divide as kek
+result1 = lol(69, 3)
+
+result2 = lol(3, 0)
+
+result3 = kek(49, 7)
+
+result4 = kek(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
